b1d9800860171d5f80d060b5d9f097c5212b45c2
Rucky0 <zugo789@hotmail.com>
2024-11-11 15:50:53 +0100

    theory

--- main.m
+++ main.m
@@ -1,24 +1,25 @@
 birthdate = 20010930;   % Write the birth date on format yyyymmdd for oldest member
 format compact
-[lambda1,lambda2,mu1,mu2,V1,V2,V] = [15,9,15,19,10,14,21]% getFerrydata(birthdate);  % Do not clear or redefine these variables.
+[lambda1,lambda2,mu1,mu2,V1,V2,V] = deal(15,9,15,19,10,14,21);%getFerrydata(birthdate);  % Do not clear or redefine these variables.
 h=0.001; % Discretization step
 
 %% ANALYTIC SOLUTION
 % Question 1 should be answered in the report
 
 % Question 2 should be answered in the report, and submitted below
+Q = @(n1, n2)[-(lambda1+lambda2), lambda2, lambda1, 0; ...
+       n2* mu2, -(lambda2+n2*mu2), 0, lambda1; ...
+       n1 * mu1, 0, -(n1*mu1+lambda2), lambda2; ...
+       0, n1*mu1, n2*mu2, -(n1*mu1+n2*mu2)];
 
-Qi = [-(lambda1+lambda2), lambda2, lambda1, 0; ...
-       mu2, -(lambda2+mu2), 0, lambda1; ...
-       mu1, 0, -(mu1+lambda2), lambda2; ...
-       0, mu1, mu2, -(mu1+mu2)];
+Qi = Q(3,0);
 Qieig=sort(eig(Qi)); % We compare the eigenvalues
 % To make sure that the order of your states will not change the result
-Qii = Qi^2;
+Qii = Q(2,1);
 Qiieig=sort(eig(Qii));
-Qiii = Qi^3; 
+Qiii = Q(1,2); 
 Qiiieig=sort(eig(Qiii));
-Qiv = Qi^4; 
+Qiv = Q(0,3); 
 Qiveig=sort(eig(Qiv));
 
 %%
@@ -26,22 +27,21 @@ Qiveig=sort(eig(Qiv));
 % Question 3 should be answered in the report
 
 % Question 4 should be answered in the report, describe how you do it, and  
-
-PIi  = "to do" 
+PIi  = stationary_states(Qi);
 PIisort=sort(PIi); % We compare the sorted vectors
-PIii = "to do" 
+PIii = stationary_states(Qii);
 PIiisort=sort(PIii);
-PIiii = "to do" 
+PIiii = stationary_states(Qiii);
 PIiiisort=sort(PIiii);
-PIiv = "to do" 
+PIiv = stationary_states(Qiv);
 PIivsort=sort(PIiv);
 
 % Question 5 should be answered in the report, describe how you do it, and  
-
-AVi = "to do" 
-AVii = "to do" 
-AViii = "to do" 
-AViv = "to do" 
+velocity = [V, V1, V2, 0];
+AVi = dot(PIi,velocity);
+AVii = dot(PIii,velocity);
+AViii = dot(PIiii,velocity);
+AViv = dot(PIiv,velocity);
 
 AV= [AVi AVii AViii AViv];
 
@@ -74,3 +74,12 @@ ETtTF = "to do"
 
 % Some of the following commands may be useful for the implementation when repeating steps over and over
 % for, while, switch, break     
+
+function pi = stationary_states(Q)
+    
+    b = [zeros(4,1);1];
+    A = [Q'; ones(1, size(Q, 1))];
+    pi  = A\b;
+    pi = pi';
+
+end
